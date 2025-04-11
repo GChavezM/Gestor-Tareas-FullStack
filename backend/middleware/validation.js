f76214977a34a77e4b260a7a281b8eef60b84f09
Gabriel Chávez <gchavezmardonez@gmail.com>
2025-04-10 23:57:34 -0400

    Adds validation for tasks and queries

--- backend/middleware/validation.js
+++ backend/middleware/validation.js
@@ -1,5 +1,5 @@
-const { body, validationResult } = require('express-validator');
-
+const { query, body, validationResult } = require('express-validator');
+const { STATUS } = require('../constants/constants');
 exports.validate = (method) => {
 	switch (method) {
 		case 'login': {
@@ -7,7 +7,7 @@ exports.validate = (method) => {
 				body('email')
 					.isEmail()
 					.withMessage('Email is not valid')
-					.toLowerCase(),
+					.normalizeEmail(),
 				body('password').notEmpty().withMessage('Password is required'),
 			];
 		}
@@ -25,7 +25,7 @@ exports.validate = (method) => {
 				body('email')
 					.isEmail()
 					.withMessage('Email is not valid')
-					.toLowerCase(),
+					.normalizeEmail(),
 				body('password')
 					.notEmpty()
 					.withMessage('Password is required')
@@ -35,6 +35,74 @@ exports.validate = (method) => {
 					.withMessage(
 						'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
 					),
+			];
+		}
+		case 'createTask': {
+			return [
+				body('title')
+					.notEmpty()
+					.withMessage('Title is required')
+					.isString()
+					.withMessage('Title must be a string')
+					.trim(),
+				body('description')
+					.optional()
+					.isString()
+					.withMessage('Description must be a string')
+					.trim(),
+				body('status')
+					.optional()
+					.isIn([
+						STATUS.PENDING,
+						STATUS.IN_PROGRESS,
+						STATUS.COMPLETED,
+					])
+					.withMessage('Status is not valid'),
+				body('deadline')
+					.optional()
+					.isDate()
+					.withMessage('Deadline must be a valid date'),
+			];
+		}
+		case 'updateTask': {
+			return [
+				body('title')
+					.optional()
+					.isString()
+					.withMessage('Title must be a string')
+					.trim(),
+				body('description')
+					.optional()
+					.isString()
+					.withMessage('Description must be a string'),
+				body('status')
+					.optional()
+					.isIn([
+						STATUS.PENDING,
+						STATUS.IN_PROGRESS,
+						STATUS.COMPLETED,
+					])
+					.withMessage('Status is not valid'),
+				body('deadline')
+					.optional()
+					.isDate()
+					.withMessage('Deadline must be a valid date'),
+			];
+		}
+		case 'getTasks': {
+			return [
+				query('status')
+					.optional()
+					.isIn([
+						STATUS.PENDING,
+						STATUS.IN_PROGRESS,
+						STATUS.COMPLETED,
+					])
+					.withMessage('Status is not valid'),
+				query('search')
+					.optional()
+					.isString()
+					.withMessage('Search must be a string'),
 			];
 		}
 		default:
