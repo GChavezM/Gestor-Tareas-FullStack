665149bffa5c18eabb483f8e40652e99500630b2
Gabriel Chávez <gchavezmardonez@gmail.com>
2025-04-10 21:07:32 -0400

    Simplifies token verification process.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,29 +1,17 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 exports.verifyToken = (req, res, next) => {
-    let token = false;
+	const token = req.headers['authorization']?.split(' ')[1];
 
-    const authType = process.env.AUTH_TYPE || "bearer";
+	if (!token) {
+		return res.status(403).json({ message: 'No token provided' });
+	}
 
-    console.log("Verify:", authType);
-    
-    if (authType === "bearer") {
-        token = req.headers["authorization"]?.split(" ")[1];
-    }
-
-    if (authType === "cookie") {
-        token = req.cookies.token;
-    }
-
-    if (!token) {
-        return res.status(403).json({ message: "No token provided" });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-}
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded;
+		next();
+	} catch (error) {
+		return res.status(401).json({ message: 'Unauthorized' });
+	}
+};
