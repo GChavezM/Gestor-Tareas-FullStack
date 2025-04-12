44a271231fbf8cd5dced6e48cb3cc5a63b522921
Gabriel Chávez <gchavezmardonez@gmail.com>
2025-04-12 19:07:22 -0400

    Configures ESLint, Prettier, and Tailwind CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{js,jsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{js,jsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: 'var(--primary-color)',
+                background: {
+                    primary: 'var(--background-primary)',
+                    secondary: 'var(--background-secondary)',
+                },
+                border: 'var(--border)',
+                shadowbox: 'var(--card-shadow)',
+                status: {
+                    pending: 'var(--status-pending)',
+                    progress: 'var(--status-progress)',
+                    completed: 'var(--status-completed)',
+                },
+            },
+        },
+    },
+    plugins: [],
 };
