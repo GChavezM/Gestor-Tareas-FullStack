40c6b83d078a60a722c3d6d83c50a8a2734d014c
Gabriel Chávez <gchavezmardonez@gmail.com>
2025-04-12 23:33:20 -0400

    Adds new color variables for UI elements

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,12 +4,22 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: 'var(--primary-color)',
+                primary: {
+                    DEFAULT: 'var(--primary-color)',
+                    hover: 'var(--primary-color-hover)',
+                },
                 background: {
                     primary: 'var(--background-primary)',
                     secondary: 'var(--background-secondary)',
                 },
                 border: 'var(--border-color)',
+                text: {
+                    primary: 'var(--text-primary)',
+                    secondary: 'var(--text-secondary)',
+                },
+                input: {
+                    background: 'var(--input-background)',
+                },
                 shadowbox: 'var(--card-shadow)',
                 status: {
                     pending: 'var(--status-pending)',
